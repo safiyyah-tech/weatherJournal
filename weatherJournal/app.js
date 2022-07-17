7a0b11747d320d2482f03598d1da12d7321c68f7
toma <tomajohn316@yahoo.com>
2022-07-17 16:18:52 +0100

    Add api calls

--- weatherJournal/app.js
+++ weatherJournal/app.js
@@ -1,18 +1,38 @@
 /* personal API key */
-const apiKey = '&appid=ad36907d7d93f570ed819c6cac9b3835&units=imperial';
+const apiKey = '&appid=b63e0438b03c686d5dde10524bc54930&units=imperial';
 
 // The DOM elements
 const zip = document.querySelector('#zip');
-const feelings = document.getElementById('#feelings');
+const feelings = document.getElementById('feelings');
 const generate = document.getElementById('generate');
+const date = document.querySelector('#date');
+const temp = document.querySelector('#temp');
+const content = document.querySelector('#content');
+const zipCode = document.querySelector('#zipcode'); 
 
 // creating a new date dynamicallly with JS
 const today = new Date();
 const newDate = today.toDateString();
-const temp = document.querySelector('#temp');
 // The country code is not needed since the default country is U.S.A
 const instance = 'http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}'
-const baseURL = 'http://api.openweathermap.org/geo/1.0/zip?zip='
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
 
 
 // Generating the event listening function.
+
+const getWeather = async (zipCode) => {
+  const response = fetch(baseURL+zipCode+apiKey);
+  const data = (await response).json();
+
+  return await data;
+};
+
+generate.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const data = await getWeather(zip.value.trim());
+  date.innerHTML = newDate;
+  content.textContent = feelings.value;
+  temp.innerHTML = data.main.temp + 'Fahrenheit';
+  zipCode.innerHTML = zip.value;
+});
+
